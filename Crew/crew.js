c8879bb3eef0495b18c7daa250f2ad0517e3f1e6
Oyunbi <camille.legloanic@laposte.net>
2023-03-22 09:05:54 +0100

    Début du DOM pour le Crew

---
+++ Crew/crew.js
@@ -0,0 +1,36 @@
+const wildersList = document.querySelector("#wilders");
+
+const adoptWilders = [
+    {
+    wName: "Quentin Silmain",
+    wImage: "../Assets/Dugolemwave.png",
+    wAlt: "Photo de Quentin",
+    wDescription: "Panda roux, responsable du Crewstibat, Quentin est polyvalent et adepte du bien nommé ChatGPT. Il s'est occupé du menu burger du site et de la partie GitHub.",
+    wObjectif: ["Landscaping", "Elder Scrolls", "L'espace"],
+    wButton: "Adopte un Wilder !"
+    },
+    {
+        wName: "Camille le Gloanic",
+        wImage: "../Assets/realcamille.png",
+        wAlt: "Photo de Camille",
+        wDescription: "",
+        wObjectif: ["Livres", "Chats", "Gateaux"],
+        wButton: "Adopte un Wilder !"
+        },
+    {
+        wName: "Ilias Ryffel dit La Grogne",
+        wImage: "../Assets/realIlias.png",
+        wAlt: "Photo de Ilias",
+        wDescription: "Ilias est un hacker de l'extrême : capuche, bonnet, son visage est toujours camouflé par un couvre-chef. D'une volonté a toute épreuve, quand il a décidé de faire un caroussel, il le fait.",
+        wObjectif: ["Barbe a papa", "Licornes", "Fission nucélaire"],
+        wButton: "Adopte un Wilder !"
+        },
+    {
+        wName: 
+        wImage: 
+        wAlt:
+        wDescription:
+        wObjectif:
+        wButton:
+        }
+]
